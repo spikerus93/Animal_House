60bea27cc0001136112ce5fdfc58df2fc91d237e
spikerus93 <spikerus93@rambler.ru>
2024-05-08 16:05:25 +0300

    Обновление данных интерфейса, добавлено в MainMenu пункты возможностей приложения.

--- src/ru/gb/Animal_House/view/MainMenu.java
+++ src/ru/gb/Animal_House/view/MainMenu.java
@@ -1,7 +1,9 @@
 package ru.gb.Animal_House.view;
 
-import ru.gb.Animal_House.view.commands.Command;
+import ru.gb.Animal_House.view.commands.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainMenu {
@@ -9,6 +11,18 @@ public class MainMenu {
     private List<Command> commandList;
 
     public MainMenu(ConsoleUI consoleUI) {
+        commandList = new ArrayList<>();
+        commandList.add(new AddAnimal(consoleUI));
+        commandList.add(new AddNewCommand(consoleUI));
+        commandList.add(new ListCommands(consoleUI));
+        commandList.add(new Counter(consoleUI));
+        commandList.add(new SortByName(consoleUI));
+        commandList.add(new SortByBirthDate(consoleUI));
+        commandList.add(new Save(consoleUI));
+        commandList.add(new Load(consoleUI));
+        commandList.add(new Remove(consoleUI));
+        commandList.add(new Finish(consoleUI));
+
 
     }
 
@@ -24,7 +38,7 @@ public class MainMenu {
         return commandList.size();
     }
 
-    public void execute(int choice) {
+    public void execute(int choice) throws IOException {
         Command command = commandList.get(choice-1);
         command.execute();
     }
