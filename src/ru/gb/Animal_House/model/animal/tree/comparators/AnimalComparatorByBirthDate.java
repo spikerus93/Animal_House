f5ff44208a6c82e8fa7d9b9f55b66bdad840e31c
spikerus93 <spikerus93@rambler.ru>
2024-05-08 16:11:37 +0300

    Внесение исправлений в работу компаратора.

--- src/ru/gb/Animal_House/model/animal/tree/comparators/AnimalComparatorByBirthDate.java
+++ src/ru/gb/Animal_House/model/animal/tree/comparators/AnimalComparatorByBirthDate.java
@@ -4,7 +4,7 @@ import ru.gb.Animal_House.model.animal.tree.TreeNode;
 
 import java.util.Comparator;
 
-public class AnimalComparatorByBirthDate<E extends TreeNode<E>> implements Comparator<E> {
+public class AnimalComparatorByBirthDate<E extends TreeNode> implements Comparator<E> {
     @Override
     public int compare(E o1, E o2) {
         return (o1.getBirthDate().compareTo(o2.getBirthDate()));
